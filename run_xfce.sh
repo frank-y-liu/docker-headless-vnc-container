61cd02f7509dc9fbe62122f56edc6f41fdc6e25c
Frank Liu <frank-y-liu@users.noreply.github.com>
2020-12-13 14:04:43 -0600

    Revised script

--- run_xfce.sh
+++ run_xfce.sh
@@ -2,6 +2,7 @@
 
 IMAGE=frankliu1/ubuntu:bionic_xfce_novnc
 echo "visit http://localhost:6901, password = vncpassword"
+echo "http://localhost:6901?password=vncpassword"
 exec docker run -d -p 5901:5901 -p 6901:6901 -v "$PWD":/source "$IMAGE" "$@"
 
 
